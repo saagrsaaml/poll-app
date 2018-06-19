49b8440c745ffea07017291de063ffdc6cab6254
vbox <saagrsaaml@gmail.com>
2018-06-19 18:08:24 +0545

    Project 2: register models in django admin

--- polls/models.py
+++ polls/models.py
@@ -1,12 +1,19 @@
+import datetime
+
 from django.db import models
+from django.utils import timezone
 
 
 class Question(models.Model):
+    question = models.Manager()
     question_text = models.CharField(max_length=200)
     pub_date = models.DateTimeField('date published')
 
     def __str__(self):
-        return str(self.pub_date)
+        return str(self.question_text)
+
+    def was_published_recently(self):
+        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
 
 
 class Choice(models.Model):
@@ -16,3 +23,6 @@ class Choice(models.Model):
 
     def __str__(self):
         return str(self.choice_text)
+
+
+
