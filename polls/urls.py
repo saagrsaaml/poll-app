503f12502ac74b36125cadb7a129f55f8cabf8ae
vbox <saagrsaaml@gmail.com>
2018-06-20 15:17:15 +0545

    Part 3: using namespace

--- polls/urls.py
+++ polls/urls.py
@@ -2,14 +2,15 @@ from django.urls import path
 
 from . import views
 
+app_name = 'polls'
 urlpatterns = [
     # ex: /polls/
     path('', views.index, name='index'),
     # ex: /polls/5
-    path('<int:question_id>/', views.detail, name="detail"),
+    path('<int:question_id>/', views.detail, name="question_detail"),
     # ex: polls/5/results
-    path('<int:question_id>/results', views.results, name='results'),
+    path('<int:question_id>/results', views.results, name='question_results'),
     # ex: /polls/5/vote/
-    path('<int:question_id>/vote/', views.vote, name='vote')
+    path('<int:question_id>/vote/', views.vote, name='question_vote')
 
 ]
