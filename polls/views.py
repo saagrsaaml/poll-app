5e37b7ffd7d829b54e3e924ca21078e75863bdb6
vbox <saagrsaaml@gmail.com>
2018-06-21 15:04:56 +0545

    add detail page

--- polls/views.py
+++ polls/views.py
@@ -43,6 +43,19 @@ from polls.models import Question, Choice
 #     question = get_object_or_404(Question, pk=question_id)
 #     return render(request, 'polls/results.html', {'question': question})
 #
+# def vote(request, question_id):
+#     question = get_object_or_404(Question, pk=question_id)
+#     try:
+#         selected_choice = question.choice_set.get(pk=request.POST['choice'])
+#     except (KeyError, Choice.DoesNotExist):
+#         return render(request, 'polls/detail.html', {
+#             'question': question,
+#             'error_message': "You didn't select a choice",
+#         })
+#     else:
+#         selected_choice.votes +=1
+#         selected_choice.save()
+#         return HttpResponseRedirect(reverse('polls:question_results', args=(question.id,)))
 
 
 # Class Base View
